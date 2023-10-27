e6f12479c1127e171790dcaed8503927788c47f7
NazarTsar <146090679+NazarTsar@users.noreply.github.com>
2023-10-27 10:15:50 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int countOnes(int n, int depth) {
+    if (n == 0) {
+        return 0;
+    }
+    else {
+        std::cout << "Глибина рекурсії: " << depth << ", Рівень рекурсії: " << n << std::endl;
+        return 1 + countOnes(n & (n - 1), depth + 1);
+    }
+}
+
+int main() {
+    int n = 37;
+    int result = countOnes(n, 0);
+    std::cout << "Кількість одиниць в двійковому представленні числа " << n << ": " << result << std::endl;
+    return 0;
+}
